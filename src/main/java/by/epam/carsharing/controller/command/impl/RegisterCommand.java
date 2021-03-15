85ea3aabe816a2be18bb1da7bad64907514fee50
Saviorich <defalt3874@gmail.com>
2021-03-15 23:24:05 +0300

    add logger

--- src/main/java/by/epam/carsharing/controller/command/impl/RegisterCommand.java
+++ src/main/java/by/epam/carsharing/controller/command/impl/RegisterCommand.java
@@ -6,6 +6,9 @@ import by.epam.carsharing.service.ServiceFactory;
 import by.epam.carsharing.service.UserService;
 import by.epam.carsharing.util.RequestParameter;
 import by.epam.carsharing.util.SessionAttribute;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +18,7 @@ import java.io.IOException;
 
 public class RegisterCommand implements Command {
     private static final ServiceFactory serviceFactory = ServiceFactory.getInstance();
-
+    private static final Logger logger = LogManager.getLogger(RegisterCommand.class);
     private static final String GO_TO_NEWS_PAGE = "Controller?command=gotonewspage";
     private static final String GO_TO_REGISTER_PAGE = "Controller?command=gotoregisterpage";
 
@@ -32,6 +35,7 @@ public class RegisterCommand implements Command {
             response.sendRedirect(GO_TO_NEWS_PAGE);
             session.setAttribute(SessionAttribute.USER, userService.findUserByEmailAndPassword(email, password).get());
         } catch (ServiceException e) {
+            logger.log(Level.DEBUG, e);
             session.setAttribute(SessionAttribute.ERROR, true);
             response.sendRedirect(GO_TO_REGISTER_PAGE);
         }

4c6b77dc8c47a7ae97bada00177887ea6ec5e12e
Saviorich <defalt3874@gmail.com>
2021-03-18 21:46:32 +0300

    change parameters in update()

--- src/main/java/by/epam/carsharing/model/dao/CarDao.java
+++ src/main/java/by/epam/carsharing/model/dao/CarDao.java
@@ -3,6 +3,10 @@ package by.epam.carsharing.model.dao;
 import by.epam.carsharing.model.entity.car.Car;
 import by.epam.carsharing.model.entity.car.CarClass;
 import by.epam.carsharing.model.dao.exception.DaoException;
+import by.epam.carsharing.model.entity.car.EngineType;
+import by.epam.carsharing.model.entity.car.GearboxType;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface CarDao extends Dao<Car> {
@@ -10,5 +14,5 @@ public interface CarDao extends Dao<Car> {
     List<Car> getCarsByYear(String year) throws DaoException;
     List<Car> getCarsByClass(CarClass carClass) throws DaoException;
 
-    void update() throws DaoException;
+    void update(int id, String brand, String model, String color, int mileage, GearboxType gearbox, String year, EngineType engineType, CarClass carClass, BigDecimal price, String imagePath) throws DaoException;
 }

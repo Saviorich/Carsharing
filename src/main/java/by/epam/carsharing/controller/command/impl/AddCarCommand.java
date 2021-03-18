7b8c5e0921ef1477b11ed5895ea8ef1594bed40e
Saviorich <defalt3874@gmail.com>
2021-03-18 21:45:43 +0300

    add new command

---
+++ src/main/java/by/epam/carsharing/controller/command/impl/AddCarCommand.java
@@ -0,0 +1,15 @@
+package by.epam.carsharing.controller.command.impl;
+
+import by.epam.carsharing.controller.command.Command;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddCarCommand implements Command {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

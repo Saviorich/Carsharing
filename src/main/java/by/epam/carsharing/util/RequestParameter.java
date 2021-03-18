70ffb334bad709479378c8cf5a02cdabea9bcaf2
Saviorich <defalt3874@gmail.com>
2021-03-18 21:50:06 +0300

    add new parameters

--- src/main/java/by/epam/carsharing/util/RequestParameter.java
+++ src/main/java/by/epam/carsharing/util/RequestParameter.java
@@ -2,30 +2,28 @@ package by.epam.carsharing.util;
 
 public final class RequestParameter {
     public static final String NEWS = "news";
-
     public static final String LOGIN = "login";
-
     public static final String EMAIL = "email";
-
     public static final String PASSWORD = "password";
-
     public static final String NAME = "name";
-
     public static final String LOCALE = "locale";
-
     public static final String ERROR = "error";
-
     public static final String COMMAND = "command";
-
     public static final String CARS = "cars";
-
+    public static final String DATA = "data";
     public static final String DATA_ID = "data_id";
-
     public static final String HEADER_EDITOR = "header_editor";
-
     public static final String CONTENT_EDITOR = "content_editor";
-
     public static final String IMAGE_PATH = "image_path";
+    public static final String BRAND_EDITOR = "brand_editor";
+    public static final String MODEL_EDITOR = "model_editor";
+    public static final String COLOR = "color_editor";
+    public static final String MILEAGE_EDITOR = "mileage_editor";
+    public static final String GEARBOX_EDITOR = "gearbox_editor";
+    public static final String YEAR_EDITOR = "year_editor";
+    public static final String ENGINE_EDITOR = "engine_editor";
+    public static final String CLASS_EDITOR = "class_editor";
+    public static final String PRICE_EDITOR = "price_editor";
 
     private RequestParameter() {}
 }

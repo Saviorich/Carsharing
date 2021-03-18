60cd63d8e35c36493939d1f449e6330a2b9fdd0d
Saviorich <defalt3874@gmail.com>
2021-03-19 00:26:40 +0300

    update code

--- src/main/webapp/js/news_edit.js
+++ src/main/webapp/js/news_edit.js
@@ -13,6 +13,11 @@ function readURL(input) {
 
 $(document).ready(function(){
     $("#view").click(function(){
+
+        alert($('#brand_editor').val() + " " + $('#model_editor').val())
+        $('.car_block__brand span').text($('#brand_editor').val() + " " + $('#model_editor').val());
+        $('.car_block__img img').attr('src', $('.editor_block__image img').attr('src'))
+
         var data = CKEDITOR.instances['content_editor'].getData();
         $('.news_block__header span').text($("#header_editor").val());
         $('.news_block__content span').html(data);

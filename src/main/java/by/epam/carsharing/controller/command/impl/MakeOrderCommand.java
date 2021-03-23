e39eb6fe56de5b573d51e327fe9f8bf9b701a1ff
Saviorich <defalt3874@gmail.com>
2021-03-23 12:57:22 +0300

    redirect user to orders page

--- src/main/java/by/epam/carsharing/controller/command/impl/MakeOrderCommand.java
+++ src/main/java/by/epam/carsharing/controller/command/impl/MakeOrderCommand.java
@@ -27,30 +27,29 @@ public class MakeOrderCommand implements Command {
     private static final ServiceFactory serviceFactory = ServiceFactory.getInstance();
     private static final Logger logger = LogManager.getLogger(MakeOrderCommand.class);
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    private static final String GO_TO_ORDERS_PAGE = "Controller?command=gotoorderspage";
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         OrderService orderService = serviceFactory.getOrderService();
         PassportService passportService = serviceFactory.getPassportService();
 
-        int userId = ((User) request.getSession().getAttribute(SessionAttribute.USER)).getId();
-        int carId = Integer.parseInt(request.getParameter(RequestParameter.DATA_ID));
 
         Date startDate = null;
         Date endDate = null;
         Date issueDate = null;
         try {
-            startDate = sdf.parse(request.getParameter("start_date"));
-            endDate = sdf.parse(request.getParameter("end_date"));
-            issueDate = sdf.parse(request.getParameter("issue_date"));
+            startDate = sdf.parse(request.getParameter(RequestParameter.START_DATE));
+            endDate = sdf.parse(request.getParameter(RequestParameter.END_DATE));
+            issueDate = sdf.parse(request.getParameter(RequestParameter.ISSUE_DATE));
         } catch (ParseException e) {
             logger.log(Level.DEBUG, e);
         }
 
-        String passportNumber = request.getParameter("passport_number");
-        String identificationNumber = request.getParameter("identification_number");
-
-        // TODO: check is car available maybe for admin
+        int userId = ((User) request.getSession().getAttribute(SessionAttribute.USER)).getId();
+        int carId = Integer.parseInt(request.getParameter(RequestParameter.DATA_ID));
+        String passportNumber = request.getParameter(RequestParameter.PASSPORT_NUMBER);
+        String identificationNumber = request.getParameter(RequestParameter.IDENTIFICATION_NUMBER);
 
         try {
             Passport passport = new Passport(passportNumber, identificationNumber, issueDate);
@@ -58,7 +57,7 @@ public class MakeOrderCommand implements Command {
             Order order = new Order(userId, carId, 0, startDate, endDate, "", "",
                     passport.getPassportNumber());
             orderService.add(order);
-            response.sendRedirect("Controller?command=gotocarspage");
+            response.sendRedirect(GO_TO_ORDERS_PAGE);
         } catch (ServiceException e){
             logger.log(Level.ERROR, e);
         }

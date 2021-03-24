ccd99dac2ab6a1dcc965a333c42517f9effefaf1
Saviorich <defalt3874@gmail.com>
2021-03-24 23:21:22 +0300

    check for user and user's role

--- src/main/java/by/epam/carsharing/controller/command/impl/GoToUsersOrdersPage.java
+++ src/main/java/by/epam/carsharing/controller/command/impl/GoToUsersOrdersPage.java
@@ -2,10 +2,13 @@ package by.epam.carsharing.controller.command.impl;
 
 import by.epam.carsharing.controller.command.Command;
 import by.epam.carsharing.model.entity.Order;
+import by.epam.carsharing.model.entity.Role;
+import by.epam.carsharing.model.entity.user.User;
 import by.epam.carsharing.model.service.OrderService;
 import by.epam.carsharing.model.service.ServiceFactory;
 import by.epam.carsharing.model.service.exception.ServiceException;
 import by.epam.carsharing.util.RequestParameter;
+import by.epam.carsharing.util.SessionAttribute;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,15 +25,23 @@ public class GoToUsersOrdersPage implements Command {
     private static final ServiceFactory serviceFactory = ServiceFactory.getInstance();
     private static final Logger logger = LogManager.getLogger(GoToUsersOrdersPage.class);
     private static final String ORDERS_PAGE = "/WEB-INF/jsp/user_orders.jsp";
+    private static final String LOGIN_PAGE = "/login";
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher(ORDERS_PAGE);
+        List<Order> orders;
+        RequestDispatcher requestDispatcher;
+        OrderService orderService = serviceFactory.getOrderService();
+        User user = (User) request.getSession().getAttribute(SessionAttribute.USER);
 
         try {
-            OrderService orderService = serviceFactory.getOrderService();
-            List<Order> orders = orderService.getAll();
-            request.setAttribute(RequestParameter.ORDERS, orders);
+            if (user != null) {
+                requestDispatcher = request.getRequestDispatcher(ORDERS_PAGE);
+                orders = user.getRole() == Role.ADMIN ? orderService.getAll() : orderService.getAllByUserId(user.getId());
+                request.setAttribute(RequestParameter.ORDERS, orders);
+            } else {
+                requestDispatcher = request.getRequestDispatcher(LOGIN_PAGE);
+            }
             requestDispatcher.forward(request, response);
         } catch (ServiceException e) {
             logger.log(Level.ERROR, e);

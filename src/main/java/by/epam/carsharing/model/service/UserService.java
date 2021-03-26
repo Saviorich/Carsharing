345e901067b5cd9e0d25de888fba020de98dfc58
Saviorich <defalt3874@gmail.com>
2021-03-26 18:35:19 +0300

    minor fixes

--- src/main/java/by/epam/carsharing/model/service/UserService.java
+++ src/main/java/by/epam/carsharing/model/service/UserService.java
@@ -6,7 +6,7 @@ import by.epam.carsharing.model.service.exception.ServiceException;
 import java.util.Optional;
 
 public interface UserService {
-    User findUserById(Integer id);
+    Optional<User> getById(Integer id) throws ServiceException;
     Optional<User> findUserByEmailAndPassword(String email, String password) throws ServiceException;
 
     void registerUser(String email, String password) throws ServiceException;

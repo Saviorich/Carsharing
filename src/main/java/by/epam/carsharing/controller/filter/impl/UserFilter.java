2eeb6f23c7be2b713ae351eda45bf7b63a5fc185
Saviorich <defalt3874@gmail.com>
2021-03-27 21:40:54 +0300

    add orders page url

--- src/main/java/by/epam/carsharing/controller/filter/impl/UserFilter.java
+++ src/main/java/by/epam/carsharing/controller/filter/impl/UserFilter.java
@@ -10,14 +10,16 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(filterName = "UserFilter", urlPatterns = { "/order" })
+@WebFilter(filterName = "UserFilter", urlPatterns = { "/Controller?command=gotoorderspage" })
 public class UserFilter extends AbstractFilter {
+
+    private static final String LOGIN_PAGE = "/login";
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         User user = getUser(servletRequest);
         if (user == null) {
-            throw new ServletException("User is not authenticated");
+            servletRequest.getRequestDispatcher(LOGIN_PAGE).forward(servletRequest, servletResponse);
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }

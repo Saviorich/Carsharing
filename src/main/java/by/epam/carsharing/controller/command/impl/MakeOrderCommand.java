4b7ec8df3641830d7b854014051f1f36babbac43
Saviorich <defalt3874@gmail.com>
2021-03-28 11:20:46 +0300

    refactor code

--- src/main/java/by/epam/carsharing/controller/command/impl/MakeOrderCommand.java
+++ src/main/java/by/epam/carsharing/controller/command/impl/MakeOrderCommand.java
@@ -4,15 +4,14 @@ import by.epam.carsharing.controller.command.Command;
 import by.epam.carsharing.model.entity.Order;
 import by.epam.carsharing.model.entity.car.Car;
 import by.epam.carsharing.model.entity.status.OrderStatus;
-import by.epam.carsharing.model.entity.user.Passport;
 import by.epam.carsharing.model.entity.user.User;
 import by.epam.carsharing.model.service.CarService;
 import by.epam.carsharing.model.service.OrderService;
-import by.epam.carsharing.model.service.PassportService;
 import by.epam.carsharing.model.service.ServiceFactory;
 import by.epam.carsharing.model.service.exception.ServiceException;
 import by.epam.carsharing.util.RequestParameter;
 import by.epam.carsharing.util.SessionAttribute;
+import by.epam.carsharing.validation.impl.OrderValidator;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -31,32 +30,41 @@ public class MakeOrderCommand implements Command {
     private static final Logger logger = LogManager.getLogger(MakeOrderCommand.class);
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     private static final String GO_TO_ORDERS_PAGE = "Controller?command=gotoorderspage";
+    private static final String GO_TO_ORDER_PAGE = "Controller?command=gotoorderpage&data_id=%d&error=%s";
+    private static final String INVALID_DATE_MESSAGE =  "Invalid date";
+    private static final String SERVICE_EXCEPTION_MESSAGE = "Something went wrong";
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        OrderValidator validator = new OrderValidator();
+        int carId = Integer.parseInt(request.getParameter(RequestParameter.DATA_ID));
         OrderService orderService = serviceFactory.getOrderService();
         CarService carService = serviceFactory.getCarService();
-
-        Date startDate = null;
-        Date endDate = null;
-        try {
-            startDate = DATE_FORMAT.parse(request.getParameter(RequestParameter.START_DATE));
-            endDate = DATE_FORMAT.parse(request.getParameter(RequestParameter.END_DATE));
-        } catch (ParseException e) {
-            logger.log(Level.DEBUG, e);
-            // TODO: show error message on page
-        }
-
         User user = ((User) request.getSession().getAttribute(SessionAttribute.USER));
 
         try {
-            int carId = Integer.parseInt(request.getParameter(RequestParameter.DATA_ID));
-            Car car = carService.getById(carId).get();
-            Order order = new Order(user, car, OrderStatus.NEW, startDate, endDate, "", "");
-            orderService.add(order);
-            response.sendRedirect(GO_TO_ORDERS_PAGE);
+            Date startDate = parseDate(request.getParameter(RequestParameter.START_DATE));
+            Date endDate = parseDate(request.getParameter(RequestParameter.END_DATE));
+            if (!validator.isCarAvailableForDates(orderService.getAll(), carId, startDate, endDate)
+                    || !validator.isDatesValid(startDate, endDate)) {
+                response.sendRedirect(String.format(GO_TO_ORDER_PAGE, carId, validator.getMessage()));
+            } else {
+                Car car = carService.getById(carId).get();
+                Order order = new Order(user, car, OrderStatus.NEW, startDate, endDate, "", "");
+                orderService.add(order);
+                response.sendRedirect(GO_TO_ORDERS_PAGE);
+            }
         } catch (ServiceException e){
             logger.log(Level.ERROR, e);
+            response.sendRedirect(String.format(GO_TO_ORDER_PAGE, carId, SERVICE_EXCEPTION_MESSAGE));
+        }
+    }
+
+    private Date parseDate(String stringDate) throws ServiceException {
+        try {
+            return DATE_FORMAT.parse(stringDate);
+        } catch (ParseException e) {
+            throw new ServiceException(INVALID_DATE_MESSAGE, e);
         }
     }
 }

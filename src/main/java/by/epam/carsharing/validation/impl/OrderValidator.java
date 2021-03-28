6c4444b0e98849f154f5ee98e6107f0207eb5c26
Saviorich <defalt3874@gmail.com>
2021-03-28 10:53:17 +0300

    validation for orders

---
+++ src/main/java/by/epam/carsharing/validation/impl/OrderValidator.java
@@ -0,0 +1,36 @@
+package by.epam.carsharing.validation.impl;
+
+import by.epam.carsharing.model.entity.Order;
+import by.epam.carsharing.util.DateUtils;
+import by.epam.carsharing.validation.Validator;
+
+import java.util.Date;
+import java.util.List;
+
+public class OrderValidator extends Validator {
+
+    private static final DateUtils DATE_UTILS = new DateUtils();
+
+    public boolean isCarAvailableForDates(List<Order> orders, int carId, Date startDate, Date endDate) {
+        boolean isAvailable =  orders.stream()
+                .filter(order -> order.getCar().getId() == carId)
+                .noneMatch(order -> DATE_UTILS.isBetweenDates(startDate, order.getStartDate(), order.getEndDate())
+                            && DATE_UTILS.isBetweenDates(endDate, order.getStartDate(), order.getEndDate()));
+        if (!isAvailable) {
+            message = DATE_UTILS.formatDate(startDate) + " : " + DATE_UTILS.formatDate(endDate);
+        }
+        return isAvailable;
+    }
+
+    public boolean isDatesValid(Date startDate, Date endDate) {
+        if (startDate.before(DATE_UTILS.getCurrentDateWithoutTime())) {
+            message = "Start date is earlier than today's date";
+            return false;
+        } else if (startDate.after(endDate)) {
+            message = "Start date is earlier than end date";
+            return false;
+        }
+        return true;
+    }
+
+}

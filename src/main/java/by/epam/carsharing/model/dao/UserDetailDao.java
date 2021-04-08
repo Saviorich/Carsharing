8aa035fe8b392f468f3bb49e9e1624b27281693a
Saviorich <defalt3874@gmail.com>
2021-04-08 10:46:07 +0300

    delete unnecessary dao and services

--- src/main/java/by/epam/carsharing/model/dao/UserDetailDao.java
+++
@@ -1,8 +0,0 @@
-package by.epam.carsharing.model.dao;
-
-import by.epam.carsharing.model.dao.exception.DaoException;
-import by.epam.carsharing.model.entity.user.UserDetail;
-
-public interface UserDetailDao extends Dao<UserDetail> {
-    void update() throws DaoException;
-}
